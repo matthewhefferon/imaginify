de90c8a543eb66f79e2bcd512ee21a515f8f9e26
Matthew Hefferon <matthefferon@gmail.com>
2024-05-16 18:59:14 -0700

    Clerk authentication

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <div className="root-container">
-        <div className="wrapper">{children}</div>
+        <div className="wrapper">
+          {children}
+        </div>
       </div>
     </main>
   );
